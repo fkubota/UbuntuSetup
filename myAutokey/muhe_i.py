5900ae3c4737beed0dff968c7312b286dfcdc00c
fkubota_office <fumihiro.kubota@hmcom.co.jp>
2019-04-05 10:36:29 +0900

    add Autokey

---
+++ myAutokey/muhe_i.py
@@ -0,0 +1,5 @@
+# Enter script code
+import time
+keyboard.send_keys("<home>")
+time.sleep(0.1)
+keyboard.send_keys("<shift>+<end>")
---
+++ myAutokey/muhe_w.py
@@ -0,0 +1,4 @@
+# Enter script code
+keyboard.send_keys("<ctrl>+<left>")
+time.sleep(0.1)
+keyboard.send_keys("<shift>+<ctrl>+<right>")
